29ac6df3ef0ef73b3bec9cacdeaf41319fdeed87
qh120411 <quanghuyy0411@gmail.com>
2026-01-25 18:05:52 +0700

    feat: add YOLO license plate detection module

---
+++ src/detect.py
@@ -0,0 +1,16 @@
+from ultralytics import YOLO
+
+model = YOLO("src/models/license_plate_detector.pt")
+
+def detect_plate(frame, conf = 0.35) :
+    res = model(frame, conf = conf, verbose = False )
+    
+    plates = []
+    
+    for r in res :
+        for box in r.boxes :
+            x1, y1, x2, y2 = map(int, box.xyxy[0])
+            plates.append((x1,y1,x2,y2))
+            
+    return plates
+
